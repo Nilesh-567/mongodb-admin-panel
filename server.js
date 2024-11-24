8f03c6f0529f7d7b2bfa618eba5b9b768511192a
Nilesh-567 <137327289+Nilesh-567@users.noreply.github.com>
2024-11-25 01:05:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import cors from "cors"; 
+import axios from "axios"; 
 import { Server } from "socket.io";
 import { fileURLToPath } from "url";
 import { checkConnection } from "./db.js"; // Ensure `db.js` is in the same directory
@@ -16,6 +17,11 @@ const __dirname = path.dirname(__filename);
 
 // Serve the React build directory
 //app.use(express.static(path.join(__dirname)));
+
+axios.get('https://lovely-treacle-c111d4.netlify.app/')
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error(error));
+
 
 // Alternatively, redirect to Netlify URL
 /*app.get('/', (req, res) => {
@@ -33,12 +39,14 @@ const server = app.listen(PORT, () => {
 });
 
 // Initialize Socket.IO
-const io = new Server(server , {
+const io = new Server(server
+  /* , {
   cors: {
     origin: 'https://lovely-treacle-c111d4.netlify.app',
     methods: ['GET', 'POST'],
   },
-});
+}  */ 
+);
 
 // Handle Socket.IO connections
 io.on("connection", (socket) => {
